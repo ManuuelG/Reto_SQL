8e06557577f1873cb600f9d2f259a9a743c0c3b5
ManuuelG <manuelguerreron93@gmail.com>
2023-11-01 19:56:15 +0100

    ejercicio 3

---
+++ SELECT-FROM-ORDER BY.sql
@@ -0,0 +1,9 @@
+USE tienda;
+
+SELECT Nombre FROM producto;
+SELECT Nombre, precio FROM producto;
+SELECT * FROM producto;
+SELECT id_fabricante from producto ORDER BY id_fabricante DESC;
+SELECT id_fabricante from producto ORDER BY id_fabricante ASC;
+SELECT nombre from producto ORDER BY nombre ASC;
+SELECT nombre from producto ORDER BY precio DESC;
